c0e1d0bd955c2b1342ee4ce9e2476fb48c8d9a39
fatma koyuncu <fatmakyc16@gmail.com>
2021-05-17 22:06:01 +0300

    initial commit

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Piece } from '../piece';
+import { Piece, King, Pawn, Castle, Knight, Bishop, Check, Move } from '../piece';
 import { Location } from '../location'
 
 @Component({
@@ -30,40 +30,40 @@ export class GameComponent implements OnInit {
       for (let j = 0; j < 8; j++) {
 
         if ((i == 0 && j == 0) || (i == 0 && j == 7)) {
-          this.location[i].push(new Location(new Piece('assets/images/castle.png')))
+          this.location[i].push(new Location(new Castle('assets/images/castle.png')))
         }
         else if (i == 0 && (j == 1 || j == 6)) {
-          this.location[i].push(new Location(new Piece("assets/images/knight.png")));
+          this.location[i].push(new Location(new Knight("assets/images/knight.png")));
         }
         else if (i == 0 && (j == 2 || j == 5)) {
-          this.location[i].push(new Location(new Piece("assets/images/bishop.png")));
+          this.location[i].push(new Location(new Bishop("assets/images/bishop.png")));
         }
         else if (i == 0 && j == 3) {
-          this.location[i].push(new Location(new Piece("assets/images/check.png")));
+          this.location[i].push(new Location(new King("assets/images/check.png")));
         }
         else if (i == 0 && j == 4) {
-          this.location[i].push(new Location(new Piece("assets/images/king.png")));
+          this.location[i].push(new Location(new Check("assets/images/king.png")));
         }
         else if (i == 1) {
-          this.location[i].push(new Location(new Piece("assets/images/pawn.png")));
+          this.location[i].push(new Location(new Pawn("assets/images/pawn.png")));
         }
         else if (i == 7 && (j == 0 || j == 7)) {
-          this.location[i].push(new Location(new Piece("assets/images/castle-w.png")));
+          this.location[i].push(new Location(new Castle("assets/images/castle-w.png")));
         }
         else if (i == 7 && (j == 1 || j == 6)) {
-          this.location[i].push(new Location(new Piece("assets/images/knight-w.png")));
+          this.location[i].push(new Location(new Knight("assets/images/knight-w.png")));
         }
         else if (i == 7 && (j == 2 || j == 5)) {
-          this.location[i].push(new Location(new Piece("assets/images/bishop-w.png")));
+          this.location[i].push(new Location(new Bishop("assets/images/bishop-w.png")));
         }
         else if (i == 7 && j == 3) {
-          this.location[i].push(new Location(new Piece("assets/images/check-w.png")));
+          this.location[i].push(new Location(new Check("assets/images/check-w.png")));
         }
         else if (i == 7 && j == 4) {
-          this.location[i].push(new Location(new Piece("assets/images/king-w.png")));
+          this.location[i].push(new Location(new King("assets/images/king-w.png")));
         }
         else if (i == 6) {
-          this.location[i].push(new Location(new Piece("assets/images/pawn-w.png")));
+          this.location[i].push(new Location(new Pawn("assets/images/pawn-w.png")));
         }
         else { this.location[i].push(new Location(new Piece(""))); }
       }
@@ -73,26 +73,47 @@ export class GameComponent implements OnInit {
 
   }
 
-  select(i,j){
-    if (this.selectedLocation) {
-      this.selectedLocation.highlight = false      
+  select(i, j) {
+    this.moveClear()
+    const pieces = this.location[i][j].piece
+    if (pieces) {
+      for (let x = 0; x < pieces.moves.length; x++) {
+        if ((i + pieces.moves[x].x) < 0 || (i + pieces.moves[x].x) > 7 || (j + pieces.moves[x].y) < 0 || (j + pieces.moves[x].y) > 7) {
+          continue
+        }
+        else {
+          this.selectedLocation = this.location[i + pieces.moves[x].x][j + pieces.moves[x].y]
+          this.selectedLocation.highlight = true
+        }        
+        if (this.selectedLocation.piece.image.length > 0) {
+          this.selectedLocation.highlight = false
+        }
+      }
     }
-    this.selectedLocation = this.location[i][j]
-    this.selectedLocation.highlight=true    
   }
 
-  drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("img");
-    ev.target.appendChild(document.getElementById(data));
+  moveClear() {
+    for (let i = 0; i < this.location.length; i++) {
+      for (let j = 0; j < this.location.length; j++) {
+        this.location[i][j].highlight = false
+      }
+
+    }
+  }
+
+  drop(i) {
+    i.preventDefault();
+    var data1 = i.dataTransfer.getData("img");
+    i.target.appendChild(document.getElementById(data1));
+    console.log(data1);
   }
 
   allowDrop(ev) {
     ev.preventDefault();
   }
 
-  drag(ev) {
-    ev.dataTransfer.setData("img", ev.target.id);
+  drag(i) {
+    i.dataTransfer.setData("img", i.target.id);
   }
 
 }
--- src/app/piece.ts
+++ src/app/piece.ts
@@ -1,68 +1,87 @@
 export class Piece{
     public image: string
-    protected moves : Array<Object>
+    public moves : Move[]
 
     constructor(image){
         this.image = image
     }
 }
 
+export interface Move{
+    x: number
+    y: number
+    null: boolean
+    move: boolean
+    take: boolean
+    eatPiece: boolean
+    repeat: number
+}
+
 export class Check extends Piece{
+    // image = "assets/images/check.png";
     moves = [
-        {x:1, y:1},
-        {x:1, y:-1},
-        {x:-1, y:1},
-        {x:-1, y:-1},
-        {x:1, y:0},
-        {x:-1, y:0},
-        {x:0, y:1},
-        {x:0, y:-1}
+        {x:1, y:1,null:false, move: false, take:false, eatPiece:true, repeat:1},
+        {x:1, y:-1,null:false, move: false, take:false, eatPiece:true, repeat:1},
+        {x:1, y:0,null:false, move: false, take:false, eatPiece:true, repeat:1},
+        {x:0, y:1,null:false, move: false, take:false, eatPiece:true, repeat:1},
+        {x:0, y:-1,null:false, move: false, take:false, eatPiece:true, repeat:1},
+        {x:-1, y:0,null:false, move: false, take:false, eatPiece:true, repeat:1},
+        {x:-1, y:1,null:false, move: false, take:false, eatPiece:true, repeat:1},
+        {x:-1, y:-1,null:false, move: false, take:false, eatPiece:true, repeat:1}
     ]
 }
 export class King extends Piece{
+    // image = "assets/images/king.png";
     moves = [
-        {x:1, y:1},
-        {x:1, y:-1},
-        {x:-1, y:1},
-        {x:-1, y:-1},
-        {x:1, y:0},
-        {x:-1, y:0},
-        {x:0, y:1},
-        {x:0, y:-1}
+        {x:1, y:1,null:false, move: false, take:false, eatPiece:true, repeat:8},
+        {x:1, y:-1,null:false, move: false, take:false, eatPiece:true, repeat:8},
+        {x:1, y:0,null:false, move: false, take:false, eatPiece:true, repeat:8},
+        {x:0, y:1,null:false, move: false, take:false, eatPiece:true, repeat:8},
+        {x:0, y:-1,null:false, move: false, take:false, eatPiece:true, repeat:8}
     ]
 }
 export class Bishop extends Piece{
+    // image = "assets/images/bishop.png";
     moves = [
-        {x:1, y:1},
-        {x:1, y:-1},
-        {x:-1, y:1},
-        {x:-1, y:-1},
+        
+        {x:1, y:-1,null:false, move: false, take:false, eatPiece:true, repeat:8},
+        {x:1, y:1,null:false, move: false, take:false, eatPiece:true, repeat:8},
+        {x:-1, y:1,null:false, move: false, take:false, eatPiece:true, repeat:8},
+        {x:-1, y:-1,null:false, move: false, take:false, eatPiece:true, repeat:8}
+        
+        
     ]
 }
 export class Knight extends Piece{
+    // image = "assets/images/knight.png";
     moves = [
-        {x:1, y:2},
-        {x:1, y:-2},
-        {x:-1, y:2},
-        {x:-1, y:-2},
-        {x:2, y:1},
-        {x:2, y:-1},
-        {x:-2, y:1},
-        {x:-2, y:-1}
+        // {x:1, y:2},
+        {x:2, y:1,null:false, move: false, take:false, eatPiece:true, repeat:1},
+        {x:2, y:-1,null:false, move: false, take:false, eatPiece:true, repeat:1},
+        {x:1, y:2,null:false, move: false, take:false, eatPiece:true, repeat:1},
+        {x:1, y:-2,null:false, move: false, take:false, eatPiece:true, repeat:1},
+        {x:-2, y:1,null:false, move: false, take:false, eatPiece:true, repeat:1},
+        {x:-2, y:-1,null:false, move: false, take:false, eatPiece:true, repeat:1},
+        {x:-1, y:2,null:false, move: false, take:false, eatPiece:true, repeat:1},
+        {x:-1, y:-2,null:false, move: false, take:false, eatPiece:true, repeat:1}
+        
     ]
 }
 export class Castle extends Piece{
+    // image = "assets/images/castle.png";
     moves = [
-        {x:1, y:0},
-        {x:-1, y:0},
-        {x:0, y:1},
-        {x:0, y:-1},
+        {x:1, y:0,null:false, move: false, take:false, eatPiece:true, repeat:8}
     ]
 }
 export class Pawn extends Piece{
+    // image = "assets/images/pawn.png";
     moves = [
-        {x:1, y:0},
-        {x:1, y:1},
-        {x:1, y:-1}
+        {x:1, y:0,null:false, move: true, take:false, eatPiece:false, repeat:1},
+        {x:2, y:0,null:false, move: false, take:false, eatPiece:false, repeat:1},
+        {x:-1, y:0,null:false, move: true, take:false, eatPiece:false, repeat:1},
+        {x:-2, y:0,null:false, move: false, take:false, eatPiece:false, repeat:1}
+        
+
+        //piyon capraz da gıdıyo, oraları ayarla, çaprazlarda tas yeme=take
     ]
 }
